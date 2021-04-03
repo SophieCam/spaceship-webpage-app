87a056deb40a7d66c3ceba56ea4d7deff691cfc6
SophieCam <79154836+SophieCam@users.noreply.github.com>
2021-04-02 22:37:06 -0400

    updated css and removed footer

--- src/app/mock-starships.ts
+++ src/app/mock-starships.ts
@@ -1,9 +1,17 @@
 import { Starship } from './starship';
 
 export const STARSHIPS: Starship[] = [
-  { id: 1, name: 'Imperial-I Class Destroyer', empire: 'Galactic Empire', length: '1600 meters', film: 'Star Wars', commander: 'Hot Shot'},
- { id: 2, name: 'Assertor-Class Dreadnought', empire: 'Galactic Empire', length: '15000 meters', film: 'Star Wars', commander: 'Dork'},
-  // { id: 3, name: 'Taylor', commander: 'English'},
+  { id: 1, name: 'Imperial-I Class Destroyer', empire: 'Galactic Empire', length: '1600 meters', film: 'Star Wars', commander: 'Hot Shot' },
+  { id: 2, name: 'Assertor-Class Dreadnought', empire: 'Galactic Empire', length: '15000 meters', film: 'Star Wars', commander: 'Dork' },
+  { id: 3, name: 'Scarran Dreadnought', empire: 'Scarran Hierarchy', length: '12208 meters', film: 'Farscape', commander: 'Scarran Hierarchy' },
+  { id: 4, name: 'Assertor-Class Dreadnought', empire: 'Galactic Empire', length: '15000 meters', film: 'Star Wars', commander: 'Dork' },
+  { id: 5, name: 'Dalek Command Sauce', empire: 'Dalek Empire', length: '3581 meters', film: 'Dr. Who', commander: 'Dr.Strange' },
+  { id: 6, name: 'Heighliner Type Jumpship', empire: 'Spacing Guild', length:  '2000 meters', film: 'Dune', commander:  'Scarran Hierarchy' },  
+  { id: 7, name: 'Leviathan', empire: 'Zerg', length: '10940 meters',film: 'StarCraft', commander:  'Jerry' },
+  { id: 8, name: 'Reverence-Class Cruiser', empire: 'Covenant Empire', length: '3000 meters', film: 'Halo', commander:  'Scorn' }, 
+  { id: 9, name: 'Athena Class Command Ship', empire: 'Terran Federation', length: '2200 meters', film: 'Startship Trooper', commander:  'Zerg' }, 
+  { id: 10, name: 'Krooze-Class Cruiser', empire: 'Orks', length: '357895 meters', film: 'Krooze-Class Cruiser', commander:  'Orks' }
+ // { id: 3, name: 'Taylor', commander: 'English'},
   // { id: 4, name: 'James', commander: 'Statistics'},
   // { id: 5, name: 'Claire', commander: 'Chemistry'},
   // { id: 6, name: 'John', commander: 'Political Science'},
